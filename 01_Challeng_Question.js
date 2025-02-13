bbf99a12c970afe4085b250cf1209a96778699c9
harryharika098 <164326284+harryharika098@users.noreply.github.com>
2025-02-13 11:27:18 +0530

    Add files via upload

---
+++ 01_Challeng_Question.js
@@ -0,0 +1,21 @@
+//write a javascript function taht changes if a number is even or odd and logs the result
+
+
+// STEP1: break the question into pices
+// STEP2: write the function
+// STEP3: it is a number which we should pass as an argument
+// STEP4: we should check if the number is even or odd by using Conditional statements
+
+// CODE
+
+function evenOdd(num) {
+    console.log(num)
+ if(num%2==0){
+    console.log(num, "THE NUMBER IS EVEN")
+ }
+ else {
+    console.log(num,"THE NUMBER IS ODD")
+ }
+
+}
+evenOdd(12);
